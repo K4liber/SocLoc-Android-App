dcf5204ebf70f2319eba706fb96613b1d07260fc
K4liber <janbielecki94@gmail.com>
2019-04-04 17:57:09 +0200

    Add localize button

--- app/src/main/java/com/example/socloc/MapsActivity.java
+++ app/src/main/java/com/example/socloc/MapsActivity.java
@@ -3,12 +3,16 @@ package com.example.socloc;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.app.ActivityCompat;
+import android.support.annotation.NonNull;
 import android.content.pm.PackageManager;
 import android.Manifest;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Button;
+import android.app.Activity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,6 +22,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationCallback;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -25,71 +34,131 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private LatLng latLng;
     private MarkerOptions markerOptions;
     private Marker marker;
-    protected LocationManager locationManager;
-    private int LOCATION_REFRESH_TIME = 1000;
-    private int LOCATION_REFRESH_DISTANCE = 0;
-    public int TAG_CODE_PERMISSION_LOCATION;
+    private Button localizeButton;
+    protected LocationCallback locationCallback;
+    protected Location userLocation;
+    private FusedLocationProviderClient fusedLocationClient;
 
-    private final LocationListener locationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(final Location location) {
-            latLng = new LatLng(location.getLatitude(),  location.getLongitude());
-            markerOptions.position(latLng);
-            marker.setPosition(latLng);
-            // mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
-        }
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-
-        }
-        @Override
-        public void onProviderEnabled(String provider) {
-
-        }
-        @Override
-        public void onProviderDisabled(String provider) {
-
-        }
-    };
+    private static final String TAG = "MapsActivity";
+    private LocationRequest locationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        android.util.Log.i(TAG, "onCreate()");
         super.onCreate(savedInstanceState);
+        allowLocationUpdates();
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        allowLocationUpdates();
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(10 * 1000); // 10 seconds
+        locationRequest.setFastestInterval(5 * 1000); // 5 seconds
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    if (location != null) {
+                        userLocation = location;
+                        moveMapToLocation();
+                        Log.i(TAG, " Location: " + userLocation);
+                    }
+                }
+            }
+        };
+
         markerOptions = new MarkerOptions();
+        this.localizeButton = this.findViewById(R.id.localize);
+        this.localizeButton.setOnClickListener((View view) -> {
+            android.util.Log.i(TAG, "Clicked!");
+            try {
+                fusedLocationClient.getLastLocation().addOnSuccessListener((Activity) view.getContext(), location -> {
+                    if (location != null) {
+                        userLocation = location;
+                        moveMapToLocation();
+                    }
+                });
+                Log.i(TAG, " Location: " + userLocation);
+            } catch (SecurityException se) {
+                Log.i(TAG, se.getMessage());
+            }
+        });
+
     }
 
     private void allowLocationUpdates() {
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
-                PackageManager.PERMISSION_GRANTED) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
-                    LOCATION_REFRESH_DISTANCE, locationListener);
-        } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
-                    TAG_CODE_PERMISSION_LOCATION
+        int locationRequestCode = 1000;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                locationRequestCode
             );
-            allowLocationUpdates();
+        } else {
+            Log.i(TAG, "PERMISSION GRANTED!");
         }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        final LatLng PERTH = new LatLng(-31.90, 115.86);
+        android.util.Log.i(TAG, "onMapReady()");
+        final LatLng PERTH = new LatLng(-11.90, 115.86);
         marker = mMap.addMarker(
             markerOptions.position(PERTH)
             .draggable(true)
             .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
         );
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        android.util.Log.i(TAG, "onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        android.util.Log.i(TAG, "onStop()");
+    }
+
+    private void moveMapToLocation() {
+        latLng = new LatLng(userLocation.getLatitude(), userLocation.getLongitude());
+        markerOptions.position(latLng);
+        marker.setPosition(latLng);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1000: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    fusedLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
+                        if (location != null) {
+                            userLocation = location;
+                        }
+                    });
+                } else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
+    }
+
 }
